95112b9473797bc349c69fa026de322f9968db8f
Matheusmp4 <mbpdias@gmail.com>
2025-06-27 21:23:08 -0300

    Build final corrigido com base e docs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/agrvai/',  // <-- ADICIONE ESSA LINHA
+  base: '/agrvai/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
